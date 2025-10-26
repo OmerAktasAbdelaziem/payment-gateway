451f25e9ebdce7deef934ef8cef474171e5bc355
OmerAktas <contact.us.omer@gmail.com>
2025-10-26 19:18:41 +0300

    Add env_file to ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '500M',
+    env_file: '.env',
     env: {
       NODE_ENV: 'production',
       DB_TYPE: 'mysql'
